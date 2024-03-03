005084683d3baabffaa3b75ca970e75af98b0df5
GonzaloFontLujan <145667252+GonzaloFontLujan@users.noreply.github.com>
2024-03-03 19:00:11 +0100

    Add files via upload

---
+++ ScriptCorreo.sh
@@ -0,0 +1,180 @@
+#!/bin/bash
+
+echo " "
+
+echo " "
+
+echo "Instalación servicio de correo"
+
+echo " "
+
+echo " "
+
+echo "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠉⢈⠩⢙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⠠⠀⠀⠨⠐⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢐⠐⠌⡌⢄⢐⢈⠔⡝⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+⣿⣿⣿⣿⣿⣿⣿⡏⠉⡀⠐⡀⢁⠈⠐⠱⠑⡑⠈⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+⣿⣿⣿⡿⢟⠩⠐⡀⠀⠀⠀⠐⠐⠁⠓⠒⠒⢀⠁⢐⢝⢟⢿⣿⣿⣿⣿⣿⣿⣿⣿
+
+⣿⣿⠫⠡⠡⠨⢀⠂⠠⠀⠀⢁⠑⡱⠛⠗⡓⢂⠠⢸⢸⢨⠣⡝⣻⣿⣿⣿⣿⣿⣿
+
+⣿⢏⢐⢁⠊⢌⠐⡈⠄⠠⠀⠀⠀⠀⠁⠑⠈⠀⢄⢕⠸⡨⠪⡪⡘⣻⣿⡿⣿⣿⣿
+
+⣿⢂⠂⡂⠅⡂⠅⡐⠨⢐⠐⠠⠠⡀⢄⠠⡠⡡⡱⡐⠕⢌⢊⢆⢣⢒⠽⢿⣿⣿⣿
+
+⠣⢂⠂⠄⠡⠐⠐⠈⠌⡐⠨⡈⠢⠨⡂⢌⢂⠆⡪⠨⡊⠂⡂⠢⢡⣢⣣⡣⣍⢿⣿
+
+⠨⢂⢂⠁⡀⠀⠀⠁⠐⠈⠐⠈⢈⠈⠐⡀⠄⠁⠌⠈⠔⣄⡀⠠⡑⡂⠆⠢⢂⠑⠽
+
+⡨⠐⠀⠀⠀⢠⡎⡀⠀⠀⠄⠈⡀⠌⠐⠠⠈⠄⡁⠂⡀⡫⠑⣑⠀⢂⠌⠄⢕⠀⠨
+
+⠺⡪⠢⡀⠀⠞⢇⢂⠀⠂⡀⠠⠀⠄⠁⠌⠨⠀⢄⠢⡁⢂⢿⡟⡀⠀⠈⠈⡀⠂⣰
+
+⢀⢀⠀⠄⠀⠀⡐⠀⡈⠄⡐⠅⡊⠌⢌⠄⡕⡑⡁⢂⠂⢂⠸⣿⡄⠀⠈⣠⣴⣿⣿
+
+⢐⠔⠠⠀⠀⡐⠠⢈⠢⢑⠄⠑⢈⠊⡂⡱⢁⣂⢌⢔⢌⢄⠀⠹⢀⣺⡿⣟⢿⣿⣿
+
+⢀⠡⠁⠂⠐⠠⠈⠄⢈⠠⢈⢢⡣⣗⠕⠄⣕⢮⣞⣞⣗⣯⢯⡷⡴⣹⡪⣷⣿⣿⣿
+
+⠊⠄⠠⠠⠡⠈⠠⢐⠠⡊⡎⣗⢭⢐⠹⡹⣮⡳⡵⣳⣻⢾⣻⣽⣻⣺⣺⣽⣿⣿⣿
+
+⣨⣾⢐⠰⠐⠅⡂⡂⢕⢜⢜⢵⢹⢑⢔⠨⢘⠸⡹⡵⣯⣻⢽⣳⣻⣺⢞⡿⣿⣿⣿
+
+⣿⣿⡔⠠⢈⠐⠐⢠⢱⢸⢸⢸⢸⠰⡡⢘⢔⢕⠝⢮⣳⢽⢝⡾⡵⡯⣏⠯⣿⣿⣿
+
+⣿⣿⣗⢅⢢⠠⠡⠢⡱⡑⡕⡕⢅⠣⡊⢨⢪⡣⡣⡂⡬⡳⢽⢽⢽⢽⣞⣧⠙⣿⣿
+
+⡻⣿⡯⡪⠢⡡⠡⢑⢌⠪⡪⡊⠆⢌⠪⢐⢕⢱⢱⢱⢱⢱⢙⢮⡫⡟⣞⢮⣳⠙⣿
+
+⠊⣿⣯⠪⡊⠄⢅⠂⢂⠁⢇⢇⢃⠂⢕⠐⠌⡲⡰⡡⣇⠇⢇⢕⠪⠉⠂⠅⠂⡑⠹
+
+⣸⢿⣳⢱⠨⡐⡽⡿⡶⡾⡬⡢⢂⠅⡢⢡⣌⠐⠈⢎⢎⢎⢔⠠⠡⠠⠠⠡⡁⡂⠡
+
+⡯⡯⡇⢅⠕⠠⢱⢹⡙⢮⢹⠨⡂⡂⢇⠌⠮⡳⠅⡂⢕⠡⡑⠠⢁⢁⣡⣡⣢⣶⣿
+
+⣗⢽⢌⡢⡡⡡⡸⡢⡣⡣⡱⡑⠔⡈⢎⢆⢂⠂⠅⣢⡳⣽⡐⢅⢂⣊⣿⣿⣿⣿⣿
+
+⣯⢯⢷⢽⢮⢯⣺⣪⢞⡮⣳⢘⠔⢌⢜⣞⣖⣮⣻⢮⣯⢷⣿⣻⣿⣿⣿⣿⣿⣿⣿
+
+⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣾⣷⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿"
+
+while (( opc != 8 ))
+do
+
+echo " "
+
+echo "  __________________________ "
+
+echo " |                          | "
+
+echo " |   MENÚ                   | "
+
+echo " |                          |  "
+
+echo " |                          |  "
+
+echo " | 1º Ver Datos de red      |  "
+
+echo " |                          |  "
+
+echo " | 2º Estado del servicio   |  " 
+
+echo " |                          |  "
+
+echo " | 3º Instalar el servicio  |  "
+
+echo " |                          |"
+
+echo " | 4º Eliminar el servicio  |"
+
+echo " |                          |"
+
+echo " | 5º Encender el servicio  |"  
+
+echo " |                          |"
+
+echo " | 6º Parar el servicio     |"    
+
+echo " |                          |"
+
+echo " | 7º Editar la conf        |"
+
+echo " |                          |"
+
+echo " | 8º Salir del programa    |"
+
+echo " |                          |"
+
+read -p " | Opción ----------------------> " opc
+
+echo " |__________________________|   "
+
+
+	case $opc in
+
+		1)
+			loop=$(ip -c a | grep "inet" | grep "lo$" | cut -d " " -f 6)
+			ipp=$(ip -c a | grep "inet" | grep "enp0s3$" | cut -d " " -f 6)
+			gate=$(ip route | grep "default" | cut -d " " -f 3)
+			echo " "
+			echo " "
+			echo "Ip loopback: $loop"
+			echo " "
+			echo "Ip de la red: $ipp"
+			echo " "
+			echo "Puerta de enlace: $gate"
+		;; 
+
+		2)
+
+			sudo systemctl status postfix
+			sudo systemctl status dovecot
+
+		;;
+		
+		3)
+
+			domin=$(grep ".com" postfix.seed |cut -d " " -f 4)
+			read -p "Escribe un dominio: " dom
+			sudo sed -i "s/$domin/$dom/g" postfix.seed
+			sudo debconf-set-selections < postfix.seed
+			sudo apt install -y postfix
+			sudo apt install dovecot-core dovecot-imapd dovecot-pop3d
+
+		;;
+
+
+		4)
+
+			sudo apt remove --purge postfix dovecot-core dovecot-imapd dovecot-pop3d
+
+		;;
+
+		5)
+
+			sudo systemctl start postfix dovecot
+
+		;;
+
+		6)
+
+			sudo systemctl stop postfix dovecot
+
+		;;
+
+		*)
+
+			echo "No es ninguna opción"
+
+		;;
+
+
+
+	esac
+
+
+done
